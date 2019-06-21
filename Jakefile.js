69a370481d530dc167e94ad8d0e3ffb74e8719d7
Brian Gibson <bggibson@hotmail.com>
2019-06-21 17:17:22 -0400

    Uses semver to check Node versions

--- Jakefile.js
+++ Jakefile.js
@@ -1,6 +1,8 @@
 (function () {
     "use strict";
 
+    const semver = require("semver");
+
     desc("Default task");
     task("default", ["version"], function () {
         console.log("\n\nBUILD OK");
@@ -10,9 +12,9 @@
     task("version", function () {
         console.log("Checking Node version: .");
         const packageJson = require("./package.json")
-        const expectedVersion = "v" + packageJson.engines.node;
+        const expectedVersion = packageJson.engines.node;
         const actualVersion = process.version;
-        if (actualVersion !== expectedVersion)
+        if (semver.neq(expectedVersion, actualVersion))
             fail("Incorrect Node version: Expected " + expectedVersion + ", but was " + actualVersion)
     });
 
