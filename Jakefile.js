57c64c61335c1fa99a13e58ba44fb52fa4346fd1
Brian Gibson <bggibson@hotmail.com>
2019-06-21 17:10:02 -0400

    Checks Node version

--- Jakefile.js
+++ Jakefile.js
@@ -2,8 +2,18 @@
     "use strict";
 
     desc("Default task");
-    task("default", function () {
+    task("default", ["version"], function () {
         console.log("\n\nBUILD OK");
+    });
+
+    desc("Check Node Version");
+    task("version", function () {
+        console.log("Checking Node version: .");
+        const packageJson = require("./package.json")
+        const expectedVersion = "v" + packageJson.engines.node;
+        const actualVersion = process.version;
+        if (actualVersion !== expectedVersion)
+            fail("Incorrect Node version: Expected " + expectedVersion + ", but was " + actualVersion)
     });
 
 }());
