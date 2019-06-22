289502dbca1d18eed5d716f9cecd677311b69280
Brian Gibson <bggibson@hotmail.com>
2019-06-22 16:40:15 -0400

    Reformats

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,17 @@
   var series = require("gulp").series;
   var semver = require("semver");
 
-  function build(done) {
-    console.log("\n\nBUILD OK");
-    done();
-  }
-
   function version(done) {
     console.log("Checking Node version: .");
-
     var packageJson = require("./package.json");
     var expectedVersion = packageJson.engines.node;
     var actualVersion = process.version;
     done(semver.neq(expectedVersion, actualVersion));
+  }
+
+  function build(done) {
+    console.log("\n\nBUILD OK");
+    done();
   }
 
   exports.default = series(version, build);
