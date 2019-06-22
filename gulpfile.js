0728c8950d0b97a5484fd44ffbd16105b0555ee0
Brian Gibson <bggibson@hotmail.com>
2019-06-22 19:43:14 -0400

    Implements lint task and adds to default task series

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,31 @@
 (function() {
   "use strict";
 
-  var series = require("gulp").series;
   var semver = require("semver");
+  var src = require("gulp").src;
+  var dest = require("gulp").dest;
+  var series = require("gulp").series;
+  var eslint = require("gulp-eslint");
+  var gulpIf = require("gulp-if");
 
   function version(done) {
-    console.log("Checking Node version: .");
+    console.log("Checking Node version:");
     var packageJson = require("./package.json");
     var expectedVersion = packageJson.engines.node;
     var actualVersion = process.version;
     done(semver.neq(expectedVersion, actualVersion));
+  }
+
+  function lint() {
+    console.log("ESLint/Prettier-ing JavaScript:");
+    return src(["./gulpfile.js"])
+      .pipe(eslint({ fix: true }))
+      .pipe(eslint.format())
+      .pipe(gulpIf(isFixed, dest("./")));
+  }
+
+  function isFixed(file) {
+    return file.eslint != null && file.eslint.fixed;
   }
 
   function build(done) {
@@ -17,5 +33,5 @@
     done();
   }
 
-  exports.default = series(version, build);
+  exports.default = series(version, lint, build);
 })();
