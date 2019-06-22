38c7bbab4b8a2a069c0fed82bf518c2f4974ef92
Brian Gibson <bggibson@hotmail.com>
2019-06-22 13:46:58 -0400

    Uses semver to check Node versions

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,21 @@
     "use strict";
 
     const {series} = require('gulp');
+    const semver = require("semver");
 
     function build(done) {
         console.log("\n\nBUILD OK");
         done();
     }
 
-    exports.default = series(build);
+    function version(done) {
+        console.log("Checking Node version: .");
+        const packageJson = require("./package.json");
+        const expectedVersion = packageJson.engines.node;
+        const actualVersion = process.version;
+        done(semver.neq(expectedVersion, actualVersion));
+    }
+
+    exports.default = series(version, build);
 
 }());
