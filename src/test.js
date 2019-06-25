e87f5ee33d936aa9ca6d6386f85866a5a3a0aa18
Brian Gibson <bggibson@hotmail.com>
2019-06-25 11:31:30 -0400

    Adds karma task to node scripts; Changes files to lint globs; Creates seperate testing tasks for node and browser; Chains testing tasks on to build task

---
+++ src/test.js
@@ -0,0 +1,11 @@
+(function () {
+    "use strict";
+
+    const assert = require("chai").assert;
+
+    describe("Some text", function () {
+        it("more text", function () {
+            assert.equal(7, 7);
+        });
+    });
+})();
