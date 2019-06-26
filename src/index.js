6f0ba332a7f5802ebaa35f2a0ecd58950cdf7ab0
Brian Gibson <bggibson@hotmail.com>
2019-06-26 16:28:27 -0400

    Changes webpack to use config file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
   "use strict";
   const addition = require("./addition.js");
   console.log("Webpack Testing");
-  console.log("9 + 16 = " + addition.add(9, 16));
+  console.log("10 + 16 = " + addition.add(10, 16));
 })();
