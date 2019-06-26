c339baa5e13fc6ef352699bcbbbc87e3855aab36
Brian Gibson <bggibson@hotmail.com>
2019-06-26 15:17:33 -0400

    Further tweeks build and script

--- build-script.js
+++ build-script.js
@@ -1,12 +1,16 @@
 (function() {
   "use strict";
 
-  exports.version = function version() {
+  exports.version = () => {
     const semver = require("semver");
     const expectedVersion = require("./package.json").engines.node;
     const actualVersion = process.version;
 
     console.log("Checking Node version: .");
     semver.neq(expectedVersion, actualVersion);
+  };
+
+  exports.clean = (directory) => {
+    console.log("Deleting '" + directory + "/': .");
   };
 })();
