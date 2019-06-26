2d461d4382ec1806b93c6fc8a62c901adaedcc74
Brian Gibson <bggibson@hotmail.com>
2019-06-26 16:40:51 -0400

    Auto generates index.html via webpack

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
   "use strict";
   const addition = require("./addition.js");
   console.log("Webpack Testing");
-  console.log("10 + 16 = " + addition.add(10, 16));
+  console.log("11 + 16 = " + addition.add(11, 16));
 })();
