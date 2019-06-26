a4d5ec44298ef35e0e62db0bbe8ca6e0db7b31ca
philjackson-theBinaryBootcamp <52044535+philjackson-theBinaryBootcamp@users.noreply.github.com>
2019-06-25 22:48:20 -0400

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>hi mom</h1>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <h1>hi mom love you </h1>
+    </div>
+  );
 }
